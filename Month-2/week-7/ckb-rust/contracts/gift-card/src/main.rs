e713ae034f24accce1dbd1eb0da921fe03843b32
fadhil <134047249+FadhilMulinya@users.noreply.github.com>
2026-05-02 16:17:14 +0300

    docs: enhance comments in main.rs for better clarity on contract logic and memory allocation

--- Month-2/week-7/ckb-rust/contracts/gift-card/src/main.rs
+++ Month-2/week-7/ckb-rust/contracts/gift-card/src/main.rs
@@ -1,20 +1,20 @@
-#![cfg_attr(not(any(feature = "library", test)), no_std)]
-#![cfg_attr(not(test), no_main)]
+//  The #! means "this applies to the whole file
+//cfg_attr means "conditionally apply an attribute"
+#![cfg_attr(not(any(feature = "library", test)), no_std)] //Tells Rust: "Don't use standard library when running on CKB"
+#![cfg_attr(not(test), no_main)] //Removes the normal main() requirement when not testing
 
+// The # without "!" means only apply to the next line
 #[cfg(any(feature = "library", test))]
-extern crate alloc;
+extern crate alloc;     //Brings in memory allocation (Vec, String, etc.) only for tests/library
 
 #[cfg(not(any(feature = "library", test)))]
-ckb_std::entry!(program_entry);
+ckb_std::entry!(program_entry); //Very important — This tells CKB that program_entry() is the entry point (like main())
 #[cfg(not(any(feature = "library", test)))]
-// By default, the following heap configuration is used:
-// * 16KB fixed heap
-// * 1.2MB(rounded up to be 16-byte aligned) dynamic heap
-// * Minimal memory block in dynamic heap is 64 bytes
-// For more details, please refer to ckb-std's default_alloc macro
-// and the buddy-alloc alloc implementation.
-ckb_std::default_alloc!(16384, 1258306, 64);
 
+ckb_std::default_alloc!(16384, 1258306, 64); //Sets up memory(heap0 so you can use `Vec, String`,etc
+//inside the contract
+
+//This is the actual contract logic. CKB calls this function. Must return 0 for success.
 pub fn program_entry() -> i8 {
     ckb_std::debug!("This is a sample contract!");
 
